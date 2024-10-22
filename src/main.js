29c6fed585ec68f43542d10872c375728187a028
Mirana-19 <martysik19@gmail.com>
2024-10-22 15:30:18 +0200

    moved api key to .env

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
+const API_KEY = process.env.API_KEY;
+
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '27366068-7f092b690db13a9a2c4fde80b';
 
-async function loadData() {
-  const res = await fetch(`${BASE_URL}?key=${API_KEY}`);
+
+async function loadData(query) {
+  const res = await fetch(
+    `${BASE_URL}?key=${API_KEY}${
+      query ? `&q=${query}` : ''
+    }&image_type=photo&orientation=horizontal&safesearch=true`
+  );
+
   const { hits } = await res.json();
+  console.log(hits);
 
   return hits;
 }
 
-loadData();
+const images = loadData();
