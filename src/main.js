218d5f6ea3de716c417f93502d94f139948601bf
Mirana-19 <martysik19@gmail.com>
2024-10-22 15:05:48 +0200

    added loadData function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '27366068-7f092b690db13a9a2c4fde80b';
+
+async function loadData() {
+  const res = await fetch(`${BASE_URL}?key=${API_KEY}`);
+  const { hits } = await res.json();
+
+  return hits;
+}
+
+loadData();
